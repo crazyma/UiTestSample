d2ab3899f3b292a8dea41a7470259f6f90103f8e
Batu <batu@dcard.cc>
2024-08-05 14:34:31 +0800

    feat: first commit

---
+++ app/src/main/java/tw/com/batu/uitestsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.com.batu.uitestsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
